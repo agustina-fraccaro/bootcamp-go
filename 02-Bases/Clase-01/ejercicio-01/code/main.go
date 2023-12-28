6393d730deec3672fd4ac4846910ebee41d39596
Agustina Fraccaro <agustina.fraccaro@mercadolibre.com>
2023-12-28 13:35:13 -0300

    Ejercicios 1 y 2 primera clase

---
+++ 02-Bases/Clase-01/ejercicio-01/code/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Agustina"
+	var address string = "Vidt 1794"
+	fmt.Println("Name:", name)
+	fmt.Println("Address:", address)
+}
---
+++ 02-Bases/Clase-01/ejercicio-02/code/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var temperature float32 = 29
+	var humidity int = 83
+	var pressure float32 = 1003.5
+
+	fmt.Println("Temperature:", temperature)
+	fmt.Println("Humidity:", humidity)
+	fmt.Println("Pressure:", pressure)
+}
