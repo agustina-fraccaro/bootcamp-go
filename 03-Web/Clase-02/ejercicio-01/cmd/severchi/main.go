ec2c9378b68596c899483f5a14fc0f706bd4b598
Agustina Fraccaro <agustina.fraccaro@mercadolibre.com>
2024-01-10 17:29:46 -0300

    POST

---
+++ 03-Web/Clase-02/ejercicio-01/cmd/severchi/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"app/internal/application"
+	"fmt"
+)
+
+func main() {
+
+	app := application.NewServerChi("localhost:8080")
+	// - run
+	if err := app.Run(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
